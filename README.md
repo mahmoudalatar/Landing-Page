a4c7dbb762a51417c60e6c6707a856801cf995b9
mahmoudalatar <62507670+mahmoudalatar@users.noreply.github.com>
2021-03-16 21:38:59 +0200

    uploding the files

---
+++ README.md
@@ -0,0 +1,28 @@
+# Landing Page Project
+
+## Table of Contents
+
+-   [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## The Js Code
+
+1- the global variable
+2- the for loop to create the navbar li items and the anchor element inside the li
+3- use the li item to navigate the content by click on the section you want to go to
+4- we use the Intersection Observer to tracing the viewport and give the section the background when it in the middle of the screen.
+5- make the responsive menu and the hamburger button by using the event listener if the user clicks it will show the nave bar menu as a vertical menu. -- with CSS code
+
+## The CSS Code
+
+-   Some adjustment of the code
+    1- make the page responsive
+    2- make a responsive class if the user click it it will show the nave bar menu as a vertical menu --by using Js
+    3- make the hamburger button and if the user clicks it will change to the close icon and open the navbar menu.
---
+++ js/app.js
@@ -0,0 +1,99 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const sections = document.querySelectorAll("section");
+const sectionsContainers = document.querySelectorAll("section");
+const navBarList = document.querySelector("#navbar__list");
+const fragment = document.createDocumentFragment();
+
+/**
+ * End Global Variables
+ */
+
+// build the nav
+for (const i of sections) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.textContent = i.dataset.nav;
+    a.setAttribute("src", `#${i.id}`);
+    a.setAttribute("class", `menu__link`);
+    li.appendChild(a);
+    fragment.appendChild(li);
+}
+
+navBarList.appendChild(fragment);
+
+/**
+ * Scroll to section on link click
+ * Scroll to anchor ID using scrollTO event
+ * !Note: calling the item li her because it created after the for loop.
+ */
+const navItems = document.querySelectorAll(".menu__link");
+
+navItems.forEach((element) => {
+    let x = document.querySelector(element.getAttribute("src"));
+    element.addEventListener("click", (event) => {
+        event.preventDefault();
+        x.scrollIntoView({ behavior: "smooth", block: "center" });
+    });
+});
+
+/**
+ * Set sections as active
+ * Creat intersection Observer to tracing the viewport
+ * creat options for the observer
+ */
+
+let options = {
+    root: null,
+    rootMargin: "-370px 0px",
+    threshold: 0,
+};
+
+// Add class 'active' to section when near top of viewport
+// creat the observe and the callback function
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (
+            entry.isIntersecting &&
+            entry.target.classList !== "your-active-class"
+        ) {
+            entry.target.classList.add("your-active-class");
+        }
+
+        if (!entry.isIntersecting) {
+            entry.target.classList.remove("your-active-class");
+        }
+    });
+}, options);
+
+// use the class constainer to tracing the vewport and firing the observer
+sectionsContainers.forEach((sectionsContainers) => {
+    observer.observe(sectionsContainers);
+});
+
+// Responsive Menu
+// Hamburger button
+const menu = document.querySelector(".navbar__menu ul");
+const hamburgerContainer = document.querySelector(".hamburger_container");
+
+hamburgerContainer.addEventListener("click", () => {
+    hamburgerContainer.classList.toggle("open");
+    menu.classList.toggle("responsive");
+});
